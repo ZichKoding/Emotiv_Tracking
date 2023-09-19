41eb14b48f1c1c07955d0f39da1330ea100a593c
Chris Zichko <chriszichkocoding@gmail.com>
2023-09-18 21:22:58 -0400

    File structure setup.

---
+++ components/eeg_data_collection/connection.py
@@ -0,0 +1,13 @@
+import os
+
+from cortex import Cortex
+
+CLIENT_ID = os.environ.get('clientId')
+CLIENT_SECRET = os.environ.get('clientSecret')
+WEBSOCKET_URL = os.environ.get('WEBSOCKET_URL')
+HEADSET_NAME = os.environ.get('HEADSET_NAME')
+LICENSE = os.environ.get('license')
+
+cortex = Cortex(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
+
+print(cortex)
