1faff83ddaa086b5c7e2e607c3d7a6c1892d946b
Chris Zichko <chriszichkocoding@gmail.com>
2023-10-20 17:01:22 -0400

    Finally have made progress in connecting to data streams. Need to study the data a little bit.

--- components/eeg_data_collection/connection.py
+++ components/eeg_data_collection/connection.py
@@ -1,8 +1,9 @@
 import os
 import sys
 
-from cortex_v2.cortex import Cortex
-
+# Need to import the cortex module's class called Cortex from the cortex_v2 directory
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', 'cortex_v2')))
+from cortex import Cortex
 
 CLIENT_ID = os.environ.get('clientId')
 CLIENT_SECRET = os.environ.get('clientSecret')
@@ -12,4 +13,29 @@ LICENSE = os.environ.get('license')
 
 cortex = Cortex(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
 
-print(cortex)
+# open websocket connection
+open_conn = cortex.open()
+
+# request access
+req_access = cortex.request_access()
+
+# query headsets
+query_headsets = cortex.query_headset()
+
+# control device
+control_device = cortex.connect_headset(HEADSET_NAME)
+
+# authorize to get cortex token
+authorize = cortex.authorize()
+
+# create session
+create_session = cortex.create_session()
+
+# subscribe data
+sub_req = cortex.sub_request(streams=['eeg', 'met'])
+
+# get data
+extract_data_labels = cortex.extract_data_labels(stream_name='eeg')
+
+
+close = cortex.close()
